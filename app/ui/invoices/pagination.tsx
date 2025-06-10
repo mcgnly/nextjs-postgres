a3cda26acbc79ac399ccb698148c9e95964f2b30
Katie McGinley <katie@mcgnly.com>
2025-06-10 14:42:26 +0200

    fix type

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -11,7 +11,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
 
-  const createPageURL = (page: number) => {
+  const createPageURL = (page: string | number) => {
     const params = new URLSearchParams(searchParams.toString());
     params.set('page', page.toString());
     return `${pathname}?${params.toString()}`;
